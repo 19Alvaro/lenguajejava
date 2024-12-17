201ec7fc7683db64655863aa4e42fec6a9745162
owner <owner@owners-MacBook-Air.local>
2024-12-17 12:00:27 -0600

    first commit

---
+++ mi-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Estudent from './estudent.jsx'
+
+
+function App() {
+  return(
+
+    <Estudent/>
+
+  )
+}
+export default App
---
+++ mi-app/src/estudent.jsx
@@ -0,0 +1,10 @@
+
+function Estudent(){
+    return(
+
+        <p>My name its alvaro alonso campos</p>
+        
+    )
+}
+
+export default Estudent
